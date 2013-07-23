8c367a3ca630fea706954f0c0ec99c9747e54cf4
Mike Cullerton <webstuff@bakednotfried.com>
2013-07-23 11:39:55 -0600

    first attempt at a main template

--- views.py
+++ views.py
@@ -1,4 +1,5 @@
 from pyramid.httpexceptions import HTTPFound, HTTPNotFound
+from pyramid.renderers import get_renderer
 from pyramid.response import Response
 from pyramid.view import view_config
 from sqlalchemy.exc import DBAPIError
@@ -11,6 +12,17 @@ logger = getLogger(__name__)
 from .models import DBSession, Idea
 from .agora import agora_utils
 
+def site_layout():
+    renderer = get_renderer("templates/main_template.pt")
+    layout = renderer.implementation().macros['layout']
+    return layout
+
+# def get_footer():
+#     renderer = get_renderer("templates/footer.pt")
+#     layout = renderer.implementation().macros['layout']
+#     return layout
+
+
 @view_config(route_name='agora', renderer='templates/agora.pt')
 class agora_view(object, agora_utils):
 
@@ -18,7 +30,7 @@ class agora_view(object, agora_utils):
         logger.info("agora_view: __init__: ")
         self.request = request
         site_location = request.registry.settings['site_location']
-        self.view_params = {'project': request.registry.settings['site_name'],
+        self.view_params = {'site_name': request.registry.settings['site_name'],
                             'description': request.registry.settings['site_description'],
                             'home_url': request.route_url('agora'),
                             'add_idea_url': request.route_url('add_idea')}
@@ -33,6 +45,8 @@ class agora_view(object, agora_utils):
         ideas = self.getIdeasByDate()
         # logger.info("agora_view: __call__: ideas: %s" % str(ideas))
         view_params['ideas'] = ideas
+        view_params['layout'] = site_layout()
+        # view_params['footer'] = get_footer()
         return view_params
 
 @view_config(route_name='add_idea', renderer='templates/add_idea.pt')
@@ -41,7 +55,7 @@ class add_idea(object, agora_utils):
     def __init__(self, request):
         logger.info("add_idea: __init__: ")
         self.request = request
-        self.view_params = {'project': request.registry.settings['site_name'],
+        self.view_params = {'site_name': request.registry.settings['site_name'],
                             'description': request.registry.settings['site_description'],
                             'home_url': request.route_url('agora')}
 
@@ -80,7 +94,7 @@ class idea_view(object, agora_utils):
         self.request = request
         site_location = request.registry.settings['site_location']
         logger.info("idea_view: __init__: site_location: %s" % str(site_location))
-        self.view_params = {'project': request.registry.settings['site_name'],
+        self.view_params = {'site_name': request.registry.settings['site_name'],
                             'description': request.registry.settings['site_description'],
                             'home_url': request.route_url('agora')}
 
