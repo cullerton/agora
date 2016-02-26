4e63a29d1a3b10502b60611f2fa82ecf8cf1288c
mike cullerton <michaelc@cullerton.com>
2016-02-25 18:23:12 -0700

    removed Pyramid stuff moved SQLAlchemy session stuff to new session.py now, we only declare namespace

--- __init__.py
+++ __init__.py
@@ -1,36 +1,6 @@
-from .agora import Agora
-from .models import Author
-from .models import Base
-from .models import Idea
+from .agora import *  # Agora
+from .models import *  # Author, Idea, Base
+from .session import *  # DBSession
+from .exceptions import *
 
-from pyramid.config import Configurator
-from pyramid.renderers import JSON
-
-from sqlalchemy import engine_from_config
-from sqlalchemy.orm import (
-    scoped_session,
-    sessionmaker,
-)
-
-from zope.sqlalchemy import ZopeTransactionExtension
-
-DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
-
-__all__ = ['Agora', 'Idea', 'Author', 'DBSession', 'Base']
-
-
-def main(global_config, **settings):
-    engine = engine_from_config(settings, 'sqlalchemy.')
-    DBSession.configure(bind=engine)
-    Base.metadata.bind = engine
-    json_renderer = JSON()
-
-    config = Configurator(settings=settings)
-    config.add_renderer(None, json_renderer)
-    config.add_route('view_idea', '/ideas/{idea}', request_method='GET')
-    config.add_route('edit_idea', '/ideas/{idea}', request_method='PUT')
-    config.add_route('delete_idea', '/ideas/{idea}', request_method='DELETE')
-    config.add_route('list_ideas', '/ideas', request_method='GET')
-    config.add_route('add_idea', '/ideas', request_method='POST')
-    config.scan()
-    return config.make_wsgi_app()
+__all__ = (agora.__all__ + models.__all__ + session.__all__ + exceptions.__all__)
---
+++ session.py
@@ -0,0 +1,10 @@
+from sqlalchemy.orm import (
+    scoped_session,
+    sessionmaker,
+)
+
+from zope.sqlalchemy import ZopeTransactionExtension
+
+DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
+
+__all__ = ['DBSession']
