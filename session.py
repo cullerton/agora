e128b61bfcddb1c097cb8ebb52a10d261838beb8
mike cullerton <michaelc@cullerton.com>
2016-02-26 13:08:52 -0700

    removed dependency on hope.sqlalchemy

--- session.py
+++ session.py
@@ -1,10 +1,8 @@
-from sqlalchemy.orm import (
-    scoped_session,
-    sessionmaker,
-)
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 
-from zope.sqlalchemy import ZopeTransactionExtension
-
-DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
+engine = create_engine('sqlite:///agora.sqlite')
+DBSession = scoped_session(sessionmaker())
+DBSession.configure(bind=engine)
 
 __all__ = ['DBSession']
