2c8604e2f1034810b83c83a88f31657be31a6596
mike cullerton <michaelc@cullerton.com>
2016-03-04 10:50:41 -0700

    added agorapi reorganized

--- setup.py
+++ setup.py
@@ -1,18 +1,30 @@
 from setuptools import setup
 
+packages = [
+    'agora',
+    'agorapi',
+]
+
+requires = [
+    'sqlalchemy',
+    'pyramid',
+]
+
 setup(
     name='agora',
-    packages=['agora'],
-    version="0.1",
-    install_requires=['sqlalchemy'],
+    version="0.2",
+    packages=packages,
+    install_requires=requires,
     author='mike cullerton',
     author_email='michaelc@cullerton.com',
-    description='A forum for ideas.',
+    description='A forum for ideas',
     url='https://github.com/cullerton/agora',
-    download_url='https://github.com/cullerton/agora/tarball/0.1',
+    download_url='https://github.com/cullerton/agora/tarball/0.2',
     keywords=['academic', 'simple', 'example'],
     classifiers=[],
     entry_points="""\
+    [paste.app_factory]
+    main = agorapi:main
     [console_scripts]
     initialize_agora_db = agora.initialize_db:main
     """,
